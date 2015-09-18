8a89b035bbd1bf14781a5503f65437e535c0dd45
guzel <guzel.maksutova@yandex.ru>
2015-09-18 12:40:49 +0400

    пункт 3.3

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     cout  << "Input X, Y: ";
     int x,y;
-    cin >> x; cin >> y;
-    int s,r;
+    cin >> x;
+    cin >> y;
+    int s,r,d;
     s=x+y;
     r=x-y;
-    cout << "Result s = ";
-    cout << s;
-    cout << "\n";
-    cout << "Result r = ";
-    cout << r;
+    d=x/y;
+    cout << "Result s = " << s << "\n"
+         << "Result r = " << r << "\n"
+         << "Result d = " << d << "\n";
 }
